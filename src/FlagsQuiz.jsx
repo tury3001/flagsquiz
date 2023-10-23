3f39de8a49fbdb4849db8dfef351a54e8b583708
Alejandro De Luca <tury3001@gmail.com>
2023-10-22 22:04:28 -0300

    Import json file

--- src/FlagsQuiz.jsx
+++ src/FlagsQuiz.jsx
@@ -4,25 +4,29 @@ import { Flag } from './components/Flag';
 import { Score } from './components/Score';
 import Timer from './components/Timer';
 import { Trivia } from './components/Trivia';
-import { getQuestions } from './api/flagsAPI';
+// import { getQuestions } from './api/flagsAPI';
+import * as triviaData from './data/trivia-data.json';
 
-const options = [
-  { id: 'A', title: 'Argentina' },
-  { id: 'B', title: 'Uruguay' },
-  { id: 'C', title: 'Brasil' },
-  { id: 'D', title: 'Paraguay' }
-];
+// const options = [
+//   { id: 'A', title: 'Argentina' },
+//   { id: 'B', title: 'Uruguay' },
+//   { id: 'C', title: 'Brasil' },
+//   { id: 'D', title: 'Paraguay' }
+// ];
 
 export const FlagsQuiz = () => {
 
   useEffect(() => {
-    getQuestions();
+    console.log('Fetch questions');
+    console.log(triviaData.trivia);
+    setOptions(triviaData.trivia[2].options)
   }, [])
   
   
   const [attempts, setAttempts] = useState(0);
   const [hits, setHits] = useState(0);
   const [questionNumber, setQuestionNumber] = useState(1);
+  const [options, setOptions] = useState([]);
 
   return (
     <div className="container mx-auto bg-fqwhite">
